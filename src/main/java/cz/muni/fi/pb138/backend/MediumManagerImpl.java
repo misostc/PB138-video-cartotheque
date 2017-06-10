24535b46e84f1106cc920eefe5245df6ddee24dd
Marcel Rebro <433819@fi.muni.cz>
2017-06-10 18:43:04 +0200

    MediumManager implementation

--- src/main/java/cz/muni/fi/pb138/backend/MediumManagerImpl.java
+++ src/main/java/cz/muni/fi/pb138/backend/MediumManagerImpl.java
@@ -2,62 +2,144 @@ package cz.muni.fi.pb138.backend;
 
 import cz.muni.fi.pb138.entity.CategoryDTO;
 import cz.muni.fi.pb138.entity.MediumDTO;
+import cz.muni.fi.pb138.exceptions.MediumNotPersistedException;
+import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import javax.xml.namespace.QName;
+import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import javax.xml.xpath.XPathVariableResolver;
+import java.util.*;
 
-import static javax.xml.xpath.XPathConstants.NODE;
-import static javax.xml.xpath.XPathConstants.NODESET;
+import static javax.xml.xpath.XPathConstants.*;
 
 /**
  * Created by Michal.Babel on 01-Jun-17.
  */
 public class MediumManagerImpl implements MediumManager {
 
-    DocumentProvider documentProvider;
+    private DocumentProvider documentProvider;
 
     public MediumManagerImpl(DocumentProvider documentProvider) {
         this.documentProvider = documentProvider;
     }
 
     @Override
-    public void addMedium(MediumDTO m) throws XPathExpressionException {
-        Node nodeCategory = evaluateXpathNode("//table:table[@table:name=\""+m.getCategory().getName()+"\"]");
+    public void addMedium(MediumDTO m) {
+        Node nodeCategory = null;
+        try {
+            MapVariableResolver mapVariableResolver = new MapVariableResolver();
+            mapVariableResolver.setVariable("cat",m.getCategory().getName());
+            nodeCategory = evaluateXpathNode("//table:table[@table:name=$cat]",mapVariableResolver);
+        } catch (XPathExpressionException e) {
+            throw new MediumNotPersistedException(e);
+        }
+        try {
+            MapVariableResolver mapVariableResolver = new MapVariableResolver();
+            mapVariableResolver.setVariable("cat",m.getCategory().getName());
 
+            m.setId(
+                    evaluateXpathInt("max($doc//table:table[@table:name=$cat]/table:table-row/table:table-cell[1]/text:p/text()))",mapVariableResolver)+1
+            );
+        } catch (XPathExpressionException e) {
+            throw new MediumNotPersistedException(e);
+        }
+
+        //table-row
+        Node nodeNewRow = documentProvider.getDocument().createElement("table:table-row");
+        ((Element)nodeNewRow).setAttribute("table:style-name","ro1");
+        //table-cell ID
+        Node nodeNewID = documentProvider.getDocument().createElement("table:table-cell");
+        ((Element)nodeNewID).setAttribute("table:style-name","ce1");
+        ((Element)nodeNewID).setAttribute("office:value-type","float");
+        ((Element)nodeNewID).setAttribute("office:value",m.getCategory().getId());
+        ((Element)nodeNewID).setAttribute("calcext:value-type","float");
+        Node nodeNewIDText = documentProvider.getDocument().createElement("text:p");
+        nodeNewIDText.setNodeValue(""+m.getId());
+        nodeNewID.appendChild(nodeNewIDText);
+        nodeNewRow.appendChild(nodeNewID);
+        //table-cell value strings
+        for(int i=0;i<m.getValues().size();i++) {
+            Node nodeNewValue = documentProvider.getDocument().createElement("table:table-cell");
+            ((Element)nodeNewValue).setAttribute("table:style-name","ce3");
+            ((Element)nodeNewValue).setAttribute("office:value-type","string");
+            ((Element)nodeNewValue).setAttribute("calcext:value-type","string");
+            Node newValueText = documentProvider.getDocument().createElement("text:p");
+            newValueText.setNodeValue(m.getValues().get(i));
+            nodeNewValue.appendChild(newValueText);
+            nodeNewRow.appendChild(nodeNewValue);
+        }
+        nodeCategory.appendChild(nodeNewRow);
     }
 
     @Override
-    public void editMedium(MediumDTO m) throws XPathExpressionException {
+    public void moveMedium(MediumDTO m, CategoryDTO newCategory) {
+        Node node = null;
+        try {
+            MapVariableResolver mapVariableResolver = new MapVariableResolver();
+            mapVariableResolver.setVariable("cat",m.getCategory().getName());
+            mapVariableResolver.setVariable("id",""+m.getId());
+            node = evaluateXpathNode(
+                    "//table:table[@table:name=$cat]/table:table-row/table:table-cell[1]/text:p[text()=$id]/../..",
+                    mapVariableResolver);
+        } catch (XPathExpressionException e) {
+            throw new MediumNotPersistedException(e);
+        }
+
+        node.getParentNode().removeChild(node);
+        Node nodeNewCategory = null;
+        try {
+            MapVariableResolver mapVariableResolver = new MapVariableResolver();
+            mapVariableResolver.setVariable("cat",newCategory.getName());
+            nodeNewCategory = evaluateXpathNode("//table:table[@table:name=$cat]",mapVariableResolver);
+        } catch (XPathExpressionException e) {
+            throw new MediumNotPersistedException(e);
+        }
+        nodeNewCategory.appendChild(node);
+    }
+
+    @Override
+    public void editMedium(MediumDTO m) {
         removeMedium(m);
         addMedium(m);
     }
 
     @Override
-    public void removeMedium(MediumDTO m) throws XPathExpressionException {
-        Node node = evaluateXpathNode("//table:table[@table:name=\""
-                +m.getCategory().getName()
-                +"\"]/table:table-row/table:table-cell[1]/text:p[text()=\""
-                +m.getId()+"\"]/../..");
+    public void removeMedium(MediumDTO m) {
+        Node node = null;
+        try {
+            MapVariableResolver mapVariableResolver = new MapVariableResolver();
+            mapVariableResolver.setVariable("cat",m.getCategory().getName());
+            mapVariableResolver.setVariable("id",""+m.getId());
+            node = evaluateXpathNode(
+                    "//table:table[@table:name=$cat]/table:table-row/table:table-cell[1]/text:p[text()=$id]/../..",
+                    mapVariableResolver);
+        } catch (XPathExpressionException e) {
+            throw new MediumNotPersistedException(e);
+        }
 
         node.getParentNode().removeChild(node);
     }
 
     @Override
-    public Collection<MediumDTO> findMediumByCategory(CategoryDTO c) throws XPathExpressionException, NumberFormatException {
+    public Collection<MediumDTO> findMediumByCategory(CategoryDTO c) {
         if (!c.isValid()) {
             throw new IllegalArgumentException("Invalid category");
         }
 
-        List<MediumDTO> collection = new ArrayList<MediumDTO>();
+        List<MediumDTO> collection = new ArrayList<>();
 
         //Iterate through media
         for(int i=2;;i++) {
-            NodeList nodeList = getCategoryRow(c.getName(),i);
+            NodeList nodeList = null;
+            try {
+                nodeList = getCategoryRow(c.getName(),i);
+            } catch (XPathExpressionException e) {
+                throw new MediumNotPersistedException(e);
+            }
             if (nodeList.item(1).getFirstChild() == null) break;
             MediumDTO mediumDTO = new MediumDTO();
             //Iterate through values of the medium
@@ -73,9 +155,16 @@ public class MediumManagerImpl implements MediumManager {
     }
 
     @Override
-    public Collection<MediumDTO> findMediumByValue(String value) throws XPathExpressionException {
-        List<MediumDTO> collection = new ArrayList<MediumDTO>();
-        NodeList nodeList = evaluateXpathNodeList("//text:p[text()[contains(.,'"+value+"')]]/../..");
+    public Collection<MediumDTO> findMediumByValue(String value) {
+        List<MediumDTO> collection = new ArrayList<>();
+        NodeList nodeList = null;
+        try {
+            MapVariableResolver mapVariableResolver = new MapVariableResolver();
+            mapVariableResolver.setVariable("val",value);
+            nodeList = evaluateXpathNodeList("//text:p[text()[contains(.,$val)]]/../..",mapVariableResolver);
+        } catch (XPathExpressionException e) {
+            throw new MediumNotPersistedException(e);
+        }
 
         //iterate through media
         for (int i=0;i<nodeList.getLength();i++) {
@@ -86,6 +175,8 @@ public class MediumManagerImpl implements MediumManager {
             }
 
             mediumDTO.setId(Integer.parseInt(getCellText(nodeList.item(i).getFirstChild())));
+            //----====PRIDAT KATEGORIU====----
+            mediumDTO.setCategory(null);
         }
         return collection;
     }
@@ -100,21 +191,44 @@ public class MediumManagerImpl implements MediumManager {
     }
 
     private NodeList getCategoryRow(String category, int row) throws XPathExpressionException {
-        return evaluateXpathNodeList("//table:table[@table:name=\""+category+"\"]/table:table-row["+row+"]/table:table-cell");
+        MapVariableResolver mapVariableResolver = new MapVariableResolver();
+        mapVariableResolver.setVariable("cat",category);
+        mapVariableResolver.setVariable("row",""+row);
+        return evaluateXpathNodeList("//table:table[@table:name=$cat]/table:table-row[$row]/table:table-cell",mapVariableResolver);
     }
 
-    private NodeList evaluateXpathNodeList(String xpathString) throws XPathExpressionException {
-        return (NodeList)XPathFactory
-                .newInstance()
-                .newXPath()
-                .compile(xpathString)
-                .evaluate(documentProvider.getDocument(),NODESET);
+    private NodeList evaluateXpathNodeList(String xpathString, MapVariableResolver mapVariableResolver) throws XPathExpressionException {
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        xPath.setXPathVariableResolver(mapVariableResolver);
+        return (NodeList) xPath.compile(xpathString).evaluate(documentProvider.getDocument(),NODESET);
     }
-    private Node evaluateXpathNode(String xpathString) throws XPathExpressionException {
-        return (Node)XPathFactory
-                .newInstance()
-                .newXPath()
-                .compile(xpathString)
-                .evaluate(documentProvider.getDocument(),NODE);
+    private Node evaluateXpathNode(String xpathString, MapVariableResolver mapVariableResolver) throws XPathExpressionException {
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        xPath.setXPathVariableResolver(mapVariableResolver);
+        return (Node) xPath.compile(xpathString).evaluate(documentProvider.getDocument(),NODE);
+    }
+    private int evaluateXpathInt(String xpathString, MapVariableResolver mapVariableResolver) throws XPathExpressionException {
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        xPath.setXPathVariableResolver(mapVariableResolver);
+        return (int) xPath.compile(xpathString).evaluate(documentProvider.getDocument(),NUMBER);
+    }
+
+    private class MapVariableResolver implements XPathVariableResolver {
+        // local store of variable name -> variable value mappings
+        Map<String, String> variableMappings = new HashMap<String, String>();
+
+        // a way of setting new variable mappings
+        public void setVariable(String key, String value)  {
+            variableMappings.put(key, value);
+        }
+
+        // override this method in XPathVariableResolver to
+        // be used during evaluation of the XPath expression
+        @Override
+        public Object resolveVariable(QName varName) {
+            // if using namespaces, there's more to do here
+            String key = varName.getLocalPart();
+            return variableMappings.get(key);
+        }
     }
 }
